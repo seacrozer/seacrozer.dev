cbd94507ff5b71b23b1135ee78111dc59eab1608
seacrozer <seacrozer@gmail.com>
2023-06-28 11:08:16 +0700

    about html removed

--- server.js
+++ server.js
@@ -5,9 +5,6 @@ const app = express();
 app.get('/', (req,res) => {
 	res.sendFile(path.join(__dirname + "/index.html"));
 });
-app.get('/about' , (req ,res ) => {
-	res.sendFile(path.join(__dirname + "/about.html"));
-})
 
 app.use('/' , express.Router)
 app.listen('3000')
