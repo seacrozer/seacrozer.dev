e0b6a4cc79cbf2229cf29728ff1ea0b4cee4530b
seacrozer <seacrozer@gmail.com>
2023-11-09 21:59:52 +0700

    Add new year countdown

---
+++ script.js
@@ -0,0 +1,21 @@
+const targetDate = new Date("2023-12-31 00:00:00").getTime();
+
+const countdown = setInterval(function() {
+    const currentDate = new Date().getTime();
+    const timeRemaining = targetDate - currentDate;
+
+    const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+    if (timeRemaining < 0) {
+        clearInterval(countdown);
+        document.getElementById("countdown-container").innerHTML = "Countdown expired!";
+    }
+}, 1000);
